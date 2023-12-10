4320e771b74c4c6b6ec8bb5e1bf57c4554033de6
Weerasinghe <neranjan@techbsl.com>
2023-12-10 18:01:39 +0530

    Swagger

--- src/main/java/com/beauty/api/controller/UserController.java
+++ src/main/java/com/beauty/api/controller/UserController.java
@@ -3,10 +3,7 @@ package com.beauty.api.controller;
 import com.beauty.api.collection.User;
 import com.beauty.api.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -15,8 +12,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping(value = "getperson")
+    public String getPerson(){return "test";}
+
     @PostMapping(value = "saveperson")
-    private String save(@RequestBody User user){
+    public String save(@RequestBody User user){
         return userService.save(user);
     }
 }
