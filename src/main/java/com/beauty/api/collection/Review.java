4c2c9101ac1430e1c4d7ec397a4d268d63ee6ea3
Aloka Piyasiri <aloka.piyasiri@gmail.com>
2023-12-31 22:27:31 +0530

    Appointment API Created

---
+++ src/main/java/com/beauty/api/collection/Review.java
@@ -0,0 +1,16 @@
+package com.beauty.api.collection;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+public class Review {
+    private Integer rating;
+    private String comment;
+    private Boolean isEnabled;
+    private String type;
+}
