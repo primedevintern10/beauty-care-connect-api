843e41a03ed36e0f66f9776cebeea45eb67bf96a
primedevintern10 <primedevintern10@gmail.com>
2026-03-21 23:56:52 +0530

    ok

--- src/main/java/com/beauty/api/collection/User.java
+++ src/main/java/com/beauty/api/collection/User.java
@@ -1,8 +1,10 @@
 package com.beauty.api.collection;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.index.Indexed;
@@ -14,6 +16,8 @@ import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "Users")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class User implements UserDetails {
